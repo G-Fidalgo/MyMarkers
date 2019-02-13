e221a80e04e5615ffb1e2bee4f4026bfedb5199f
Mr.X <seniorito.x@icloud.com>
2019-02-13 17:05:58 +0100

    pointer G

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -101,13 +101,18 @@ promise.then(currentPosition => {
               type: "geojson",
               data: places
             },
-            layout: {
-              // "icon-image": "{icon}-15",
-              "text-field": "H",
-              // "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              // "text-offset": [0, 0.6],
-              // "text-anchor": "top"
-            }      
+            type: "circle",
+            paint: {
+              'circle-radius': 10,
+              'circle-color': 'red'
+            }
+            // layout: {
+            //   // "icon-image": "{icon}-15",
+            //   "text-field": "H",
+            //   // "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            //   // "text-offset": [0, 0.6],
+            //   // "text-anchor": "top"
+            // }      
           });
           
         })
